fe23e9ac2a29fa677de0ffb624b85b9006d183f9
Gobinda Das <gobinda168@gmail.com>
2020-10-23 14:03:56 +0530

    Init fast finger project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Fast Fingers </h1>
+    <div className="bg">
+      <div className="App">
+        <img
+          src="/assets/images/icon-awesome-keyboard.svg"
+          class="Icon-awesome-keyboard"
+          alt="keyboard"
+        ></img>
+        <h1 className="text-color">Fast Fingers </h1>
+        <div className="row">
+          <div className="line"></div>
+          <div className="text-color">the ultimate typing game</div>
+          <div className="line"></div>
+        </div>
+        <input placeholder="TYPE YOUR NAME" className="input-field-name" />
+        <select
+          // placeholder="DIFFICULTY LEVEL"
+          className="input-field-difficulty"
+        >
+          <option selected>DIFFICULTY LEVEL</option>
+        </select>
+        <div className="row start-game">
+          <img src="/assets/images/play.svg" alt="play button" />
+          <h1 className="text-color">START GAME</h1>
+        </div>
+      </div>
     </div>
   );
 }
