bd054e7c7d35bcc5784286a5d420a71d8de0bd87
Gobinda Das <gobinda168@gmail.com>
2020-10-28 18:43:17 +0530

    Implement username field validation

--- src/components/pages/home/home.jsx
+++ src/components/pages/home/home.jsx
@@ -5,29 +5,32 @@ import { AppHeader } from "./header/header";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AppContext } from "../../../context/AppContext";
+import { useState } from "react";
 // import { useState } from "react";
 export const Home = () => {
   const { currentUser, setCurrentUser } = useContext(AppContext);
-
+  const [error, setError] = useState(false);
   return (
     <div className="App">
       <AppHeader />
       <input
         placeholder="TYPE YOUR NAME"
         className="input-field-name"
-        onChange={(e) =>
-          setCurrentUser({ ...currentUser, name: e.target.value })
-        }
+        onChange={(e) => {
+          setCurrentUser({ ...currentUser, name: e.target.value });
+          setError(false);
+        }}
         value={currentUser.name}
       />
-
+      {error && <p style={{ color: "red" }}>Username is Required!!</p>}
       <Dropdown />
       <Link to={currentUser.name && "/game"} className="">
         <div
           className="row start-game"
-          onClick={() =>
-            localStorage.setItem("user", JSON.stringify(currentUser))
-          }
+          onClick={() => {
+            localStorage.setItem("user", JSON.stringify(currentUser));
+            !currentUser.name && setError(true);
+          }}
         >
           <img src="/assets/images/play.svg" alt="play button" />
 
